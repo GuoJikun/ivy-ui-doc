cddf87203ea797763fba6b79797ca74afa0f56d5
GuoJikun <guojikun@y-open.com>
2022-06-19 20:26:14 +0800

    feat: 优化

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -3,10 +3,6 @@ import DefaultTheme from "vitepress/theme";
 export default {
   ...DefaultTheme,
   enhanceApp({ app }) {
-    // register global components
-    // app.config.compilerOptions.isCustomElement = (tag) =>
-    //   tag.startsWith("ivy-");
-
     import("ivy-ui");
   },
 };
